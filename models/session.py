2671fbde59878b9b83a3b08c465e1b1037dd1a06
Mikadifo <mikadifopadillaheredia@gmail.com>
2025-11-17 20:51:07 -0500

    add: session endpoints

---
+++ models/session.py
@@ -0,0 +1,28 @@
+from datetime import date
+from pydantic import BaseModel, Field, field_validator
+
+class Session(BaseModel):
+    section: str = Field(..., description="Cognidy Dashboard Section")
+    completed_at: date = Field(..., description="Completion date")
+    total: int = Field(..., ge=1)
+    correct: int = Field(..., ge=1)
+
+    @field_validator("section", mode="after")
+    @classmethod
+    def non_empty_strip(cls, value: str, info) -> str:
+        value = value.strip()
+
+        if not value:
+            raise ValueError(f"{info.field_name} must not be empty")
+
+        return value
+
+    @field_validator("completed_at", mode="after")
+    @classmethod
+    def not_in_past(cls, value: date) -> date:
+        today = date.today()
+
+        if value < today:
+            raise ValueError("completed_at cannot be in the past")
+
+        return value
--- routes/sessions.py
+++ routes/sessions.py
@@ -1,12 +1,17 @@
+from datetime import datetime, timezone
 from flask_jwt_extended import get_jwt_identity, jwt_required
+from pydantic import ValidationError
+from controllers.session_controller import get_next_session_number
 from database import get_sessions_collection, get_users_collection
 from flask import Blueprint, jsonify, request
+
+from models.session import Session
 
 sessions_bp = Blueprint("sessions", __name__)
 
 @sessions_bp.route("/", methods=["GET"])
 @jwt_required()
-def get_notes():
+def get_sessions():
     username = get_jwt_identity()
 
     user = get_users_collection().find_one({"username": username})
@@ -19,5 +24,28 @@ def get_notes():
 
     return jsonify({"message": "Sessions retrieved successfully", "data": sessions}), 200
 
-# @notes_bp.route("/upload/guest", methods=["POST"])
-# def upload_guest():
+@sessions_bp.route("/add", methods=["POST"])
+@jwt_required()
+def add_session():
+    try:
+        username = get_jwt_identity()
+        data = request.get_json()
+        session = Session(**data)
+        new_session = session.model_dump()
+        user = get_users_collection().find_one({"username": username}, {"_id": 1})
+
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+
+        new_session["number"] = get_next_session_number(user["_id"])
+        new_session["completed_at"] = datetime.combine(new_session["completed_at"], datetime.min.time(), tzinfo=timezone.utc)
+        new_session["user_id"] = str(user["_id"])
+        created = get_sessions_collection().insert_one(new_session)
+
+        if created:
+            return jsonify({"message": "Session created successfully"}), 201
+        else:
+            return jsonify({"error": "Error while saving session"}), 500
+    except ValidationError as error:
+        errors = {error["loc"][0]: error["msg"] for error in error.errors()}
+        return jsonify({"error": errors}), 400
